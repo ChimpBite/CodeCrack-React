abbe4d040a71b8eac4255ecf50a38c0aeb0a4a6b
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-01-28 13:49:14 -0700

    fixed middleware error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import { persistStore } from 'redux-persist';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage/session';
@@ -16,14 +16,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, GameEngine);
 
-// const middlewares = [logger];
-
 const store = createStore(persistedReducer);
-
-// const store = createStore(
-//   persistedReducer,
-//   composeWithDevTools(applyMiddleware(...middlewares))
-// );
 
 const persistor = persistStore(store);
 
